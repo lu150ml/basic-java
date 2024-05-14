227d932f3d9580e24ee121f7c4cf7146396eb4fa
kaworunaguisa <luishenriqueribeiro3@gmail.com>
2024-05-14 01:57:18 -0300

    entendendo as variaveis

--- sitaxe_/java-anatomia-de-classes/MinhaClasse.java
+++ sitaxe_/java-anatomia-de-classes/MinhaClasse.java
@@ -1,7 +1,7 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.print ("primeira classe criada do zero");
+        final String BR ="Brasil";
  }       
 }
 //todos os nomes de classes começam com maiuscula 
-
+//se declararmos uma variavel com final no começo ela não poderar mais  ser alterada
