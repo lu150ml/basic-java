8e32dadbc03eda9f8a5bb4c0ff82e2812b2e2655
kaworunaguisa <luishenriqueribeiro3@gmail.com>
2024-05-14 01:07:53 -0300

    montando a primeira estrutura em java para melhor enendimento de nuncias da linguagem java

---
+++ sitaxe_/java-anatomia-de-classes/MinhaClasse.java
@@ -0,0 +1,7 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.print ("primeira classe criada do zero");
+ }       
+}
+//todos os nomes de classes começam com maiuscula 
+
